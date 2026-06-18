f3d046133f148cab10ac72a835f6a1396aebc409
Bartek Łukawski <bwmn.peter@gmail.com>
2026-06-18 19:56:07 +0200

    Migrate to ROS 2 Jazzy

--- src/tiago_spnav_teleop.cpp
+++ src/tiago_spnav_teleop.cpp
@@ -2,241 +2,265 @@
 
 #include <algorithm> // std::copy
 #include <string>
-#include <pluginlib/class_list_macros.h>
+#include "controller_interface/helpers.hpp"
+#include "hardware_interface/types/hardware_interface_type_values.hpp"
+#include "pluginlib/class_list_macros.hpp"
 #include <kdl_parser/kdl_parser.hpp>
 #include <kdl/frames.hpp>
+#include <kdl/chain.hpp>
+#include <kdl/chainiksolvervel_pinv.hpp>
 #include <urdf/model.h>
 
-using namespace tiago_controllers;
+using namespace spnav_controller;
 
-constexpr auto UPDATE_LOG_THROTTLE = 1.0; // [s]
+constexpr auto UPDATE_LOG_THROTTLE = 1000.0; // [ms]
 
-SpnavController::~SpnavController()
+controller_interface::CallbackReturn SpnavController::on_init()
 {
-    if (ikSolverVel)
-    {
-        delete ikSolverVel;
-        ikSolverVel = nullptr;
-    }
+  try
+  {
+    // Create the parameter listener and get the parameters
+    param_listener = std::make_shared<ParamListener>(get_node());
+    params = param_listener->get_params();
+  }
+  catch (const std::exception & e)
+  {
+    std::fprintf(stderr, "Exception thrown during init stage with message: %s \n", e.what());
+    return controller_interface::CallbackReturn::ERROR;
+  }
+
+  const std::string robot_desc_string = get_robot_description();
+
+  KDL::Tree tree;
+
+  if (!kdl_parser::treeFromString(robot_desc_string, tree))
+  {
+    RCLCPP_ERROR(get_node()->get_logger(), "Failed to construct KDL tree");
+    return controller_interface::CallbackReturn::ERROR;
+  }
+
+  KDL::Chain chain;
+
+  if (!tree.getChain(params.start_link, params.end_link, chain))
+  {
+    RCLCPP_ERROR(get_node()->get_logger(), "Failed to get chain from KDL tree");
+    return controller_interface::CallbackReturn::ERROR;
+  }
+
+  RCLCPP_INFO_STREAM(get_node()->get_logger(), "Got chain with " << chain.getNrOfJoints() << " joints and " << chain.getNrOfSegments() << " segments");
+
+  q.resize(chain.getNrOfJoints());
+
+  ik_solver_vel = std::make_unique<KDL::ChainIkSolverVel_pinv>(chain, params.ik_solver_vel_eps, params.ik_solver_vel_max_iter);
+
+  urdf::Model model;
+
+  if (!model.initString(robot_desc_string))
+  {
+    RCLCPP_ERROR(get_node()->get_logger(), "Failed to parse robot description");
+    return controller_interface::CallbackReturn::ERROR;
+  }
+
+  for (const auto & joint_name : params.arm_joint_names)
+  {
+    auto joint = model.getJoint(joint_name);
+    arm_joint_limits.emplace_back(joint->limits->lower, joint->limits->upper);
+  }
+
+  return controller_interface::CallbackReturn::SUCCESS;
 }
 
-bool SpnavController::init(hardware_interface::PositionJointInterface* hw, ros::NodeHandle &n)
+controller_interface::CallbackReturn SpnavController::on_configure(const rclcpp_lifecycle::State & previous_state)
 {
-    KDL::Tree tree;
-    std::string robot_desc_string;
+  // update the dynamic map parameters
+  param_listener->refresh_dynamic_parameters();
 
-    if (!n.getParam("/robot_description", robot_desc_string))
-    {
-        ROS_ERROR("Could not find robot_description");
-        return false;
-    }
+  // get parameters from the listener in case they were updated
+  params = param_listener->get_params();
 
-    if (!kdl_parser::treeFromString(robot_desc_string, tree))
-    {
-        ROS_ERROR("Failed to construct kdl tree");
-        return false;
-    }
+  spnav_subscription = get_node()->create_subscription<sensor_msgs::msg::Joy>(
+    "spacenav/joy", 1, std::bind(&SpnavController::spnavCallback, this, std::placeholders::_1));
 
-    std::string start_link, end_link;
+  if (!spnav_subscription)
+  {
+    RCLCPP_ERROR(get_node()->get_logger(), "Could not subscribe to /spacenav/joy");
+    return controller_interface::CallbackReturn::ERROR;
+  }
 
-    if (!n.getParam("start_link", start_link))
-    {
-        ROS_ERROR("Could not find start_link parameter");
-        return false;
-    }
-
-    if (!n.getParam("end_link", end_link))
-    {
-        ROS_ERROR("Could not find end_link parameter");
-        return false;
-    }
-
-    if (!tree.getChain(start_link, end_link, chain))
-    {
-        ROS_ERROR("Failed to get chain from kdl tree");
-        return false;
-    }
-
-    ROS_INFO("Got chain with %d joints and %d segments", chain.getNrOfJoints(), chain.getNrOfSegments());
-
-    q.resize(chain.getNrOfJoints());
-
-    double eps;
-    int maxIter;
-
-    if (!n.getParam("ik_solver_vel_eps", eps))
-    {
-        ROS_ERROR("Could not find ik_solver_vel_eps parameter");
-        return false;
-    }
-
-    if (!n.getParam("ik_solver_vel_max_iter", maxIter))
-    {
-        ROS_ERROR("Could not find ik_solver_vel_max_iter parameter");
-        return false;
-    }
-
-    ikSolverVel = new KDL::ChainIkSolverVel_pinv(chain, eps, maxIter);
-
-    urdf::Model model;
-
-    if (!model.initString(robot_desc_string))
-    {
-        ROS_ERROR("Failed to parse urdf file");
-        return false;
-    }
-
-    std::vector<std::string> arm_joint_names;
-
-    if (!n.getParam("arm_joint_names", arm_joint_names))
-    {
-        ROS_ERROR("Could not retrieve arm joint names");
-        return false;
-    }
-
-    if (!n.getParam("joy_arm_scale", joyArmScale))
-    {
-        ROS_ERROR("Could not retrieve joy arm scale");
-        return false;
-    }
-
-    if (!n.getParam("joy_gripper_increment", joyGripperIncrement))
-    {
-        ROS_ERROR("Could not retrieve joy gripper increment");
-        return false;
-    }
-
-    for (const auto & joint_name : arm_joint_names)
-    {
-        armJoints.push_back(hw->getHandle(joint_name));
-
-        armJointLimits.emplace_back(model.getJoint(joint_name)->limits->lower,
-                                    model.getJoint(joint_name)->limits->upper);
-    }
-
-    std::vector<std::string> gripper_joint_names;
-
-    if (!n.getParam("gripper_joint_names", gripper_joint_names))
-    {
-        ROS_ERROR("Could not retrieve gripper joint names");
-        return false;
-    }
-
-    for (const auto & joint_name : gripper_joint_names)
-    {
-        gripperJoints.push_back(hw->getHandle(joint_name));
-    }
-
-    spnav = n.subscribe("spacenav/joy", 1, &SpnavController::spnavCallback, this);
-
-    if (!spnav)
-    {
-        ROS_ERROR("Could not subscribe to /spacenav/joy");
-        return false;
-    }
-
-    return true;
+  return controller_interface::CallbackReturn::SUCCESS;
 }
 
-void SpnavController::update(const ros::Time& time, const ros::Duration& period)
+controller_interface::InterfaceConfiguration SpnavController::command_interface_configuration() const
 {
-    KDL::JntArray qdot(chain.getNrOfJoints());
-    KDL::Twist tw;
+  controller_interface::InterfaceConfiguration conf;
+  conf.type = controller_interface::interface_configuration_type::INDIVIDUAL;
+  conf.names.reserve(params.arm_joint_names.size() + params.gripper_joint_names.size());
 
+  for (const auto & joint_name : params.arm_joint_names)
+  {
+    conf.names.push_back(joint_name + "/" + hardware_interface::HW_IF_POSITION);
+  }
+
+  for (const auto & joint_name : params.gripper_joint_names)
+  {
+    conf.names.push_back(joint_name + "/" + hardware_interface::HW_IF_POSITION);
+  }
+
+  return conf;
+}
+
+controller_interface::InterfaceConfiguration SpnavController::state_interface_configuration() const
+{
+  return command_interface_configuration();
+}
+
+controller_interface::CallbackReturn SpnavController::on_cleanup(const rclcpp_lifecycle::State & previous_state)
+{
+  return controller_interface::CallbackReturn::SUCCESS;
+}
+
+controller_interface::CallbackReturn SpnavController::on_shutdown(const rclcpp_lifecycle::State & previous_state)
+{
+  return controller_interface::CallbackReturn::SUCCESS;
+}
+
+controller_interface::CallbackReturn SpnavController::on_activate(const rclcpp_lifecycle::State & previous_state)
+{
+  std::string out = "Initial arm pose:";
+
+  for (int i = 0; i < state_interfaces_.size(); i++)
+  {
+    const auto op = state_interfaces_[i].get_optional();
+
+    if (op.has_value())
     {
-        std::lock_guard<std::mutex> lock(mtx);
-        tw.vel = {joyAxes[0], joyAxes[1], joyAxes[2]};
-        tw.rot = {joyAxes[3], joyAxes[4], joyAxes[5]};
+      q(i) = op.value();
+      out += " " + std::to_string(q(i));
     }
-
-    if (!checkReturnCode(ikSolverVel->CartToJnt(q, tw, qdot)))
+    else
     {
-        return;
+      RCLCPP_WARN_STREAM(get_node()->get_logger(), "State interface " << state_interfaces_[i].get_name() << " has no value");
+      return controller_interface::CallbackReturn::FAILURE;
     }
+  }
 
-    KDL::JntArray q_temp(q); // look ahead in case we may have ended up in a singular point at `q`
+  RCLCPP_INFO_STREAM(get_node()->get_logger(), out);
+  return controller_interface::CallbackReturn::SUCCESS;
+}
 
-    for (int i = 0; i < armJoints.size(); i++)
+controller_interface::CallbackReturn SpnavController::on_deactivate(const rclcpp_lifecycle::State & previous_state)
+{
+  return controller_interface::CallbackReturn::SUCCESS;
+}
+
+controller_interface::CallbackReturn SpnavController::on_error(const rclcpp_lifecycle::State & previous_state)
+{
+  return controller_interface::CallbackReturn::SUCCESS;
+}
+
+controller_interface::return_type SpnavController::update(const rclcpp::Time & time, const rclcpp::Duration & period)
+{
+  KDL::JntArray qdot(params.arm_joint_names.size());
+  KDL::Twist tw;
+
+  {
+    std::lock_guard lock(mtx);
+    tw.vel = {joy_axes[0], joy_axes[1], joy_axes[2]};
+    tw.rot = {joy_axes[3], joy_axes[4], joy_axes[5]};
+  }
+
+  if (!checkReturnCode(ik_solver_vel->CartToJnt(q, tw, qdot)))
+  {
+    return controller_interface::return_type::ERROR;
+  }
+
+  KDL::JntArray q_temp(q); // look ahead in case we may have ended up in a singular point at `q`
+
+  for (int i = 0; i < params.arm_joint_names.size(); i++)
+  {
+    q_temp(i) += qdot(i) * period.seconds() * params.joy_arm_scale;
+
+    if (q_temp(i) < arm_joint_limits[i].first || q_temp(i) > arm_joint_limits[i].second)
     {
-        q_temp(i) += qdot(i) * period.toSec() * joyArmScale;
+      RCLCPP_WARN_STREAM_THROTTLE(get_node()->get_logger(), *get_node()->get_clock(), UPDATE_LOG_THROTTLE,
+      "Joint " << i << " out of limits: " << q_temp(i) <<
+      " not in [" << arm_joint_limits[i].first << ", " << arm_joint_limits[i].second << "]");
 
-        if (q_temp(i) < armJointLimits[i].first || q_temp(i) > armJointLimits[i].second)
-        {
-            ROS_WARN_THROTTLE(UPDATE_LOG_THROTTLE, "Joint %d out of limits: %f not in [%f, %f]",
-                                                   i, q_temp(i), armJointLimits[i].first, armJointLimits[i].second);
-            return;
-        }
+      return controller_interface::return_type::ERROR;
     }
+  }
 
-    KDL::JntArray qdot_temp(chain.getNrOfJoints());
+  KDL::JntArray qdot_temp(params.arm_joint_names.size());
 
-    if (!checkReturnCode(ikSolverVel->CartToJnt(q_temp, tw, qdot_temp)))
+  if (!checkReturnCode(ik_solver_vel->CartToJnt(q_temp, tw, qdot_temp)))
+  {
+    return controller_interface::return_type::ERROR;
+  }
+
+  q = q_temp; // no singular point, so update the calculated pose
+
+  for (int i = 0; i < params.arm_joint_names.size(); i++)
+  {
+    if (!command_interfaces_[i].set_value(q(i), 1))
     {
-        return;
+      RCLCPP_WARN_STREAM_THROTTLE(get_node()->get_logger(), *get_node()->get_clock(), UPDATE_LOG_THROTTLE,
+                                  "Failed to set command for joint " << state_interfaces_[i].get_name());
     }
+  }
 
-    q = q_temp; // no singular point, so update the calculated pose
+  if (joy_buttons[0])
+  {
+    for (int i = params.arm_joint_names.size(); i < params.gripper_joint_names.size(); i++)
+    {
+      auto value = state_interfaces_[i].get_optional().value() + params.joy_gripper_increment;
 
-    for (int i = 0; i < armJoints.size(); i++)
-    {
-        armJoints[i].setCommand(q(i));
+      if (!command_interfaces_[i].set_value(value, 1))
+      {
+        RCLCPP_WARN_STREAM_THROTTLE(get_node()->get_logger(), *get_node()->get_clock(), UPDATE_LOG_THROTTLE,
+                                    "Failed to set command for joint " << state_interfaces_[i].get_name());
+      }
     }
+  }
+  else if (joy_buttons[1])
+  {
+    for (int i = params.arm_joint_names.size(); i < params.gripper_joint_names.size(); i++)
+    {
+      auto value = state_interfaces_[i].get_optional().value() - params.joy_gripper_increment;
 
-    if (joyButtons[0])
-    {
-        for (int i = 0; i < gripperJoints.size(); i++)
-        {
-            gripperJoints[i].setCommand(gripperJoints[i].getPosition() + joyGripperIncrement);
-        }
+      if (!command_interfaces_[i].set_value(value, 1))
+      {
+        RCLCPP_WARN_STREAM_THROTTLE(get_node()->get_logger(), *get_node()->get_clock(), UPDATE_LOG_THROTTLE,
+                                    "Failed to set command for joint " << state_interfaces_[i].get_name());
+      }
     }
-    else if (joyButtons[1])
-    {
-        for (int i = 0; i < gripperJoints.size(); i++)
-        {
-            gripperJoints[i].setCommand(gripperJoints[i].getPosition() - joyGripperIncrement);
-        }
-    }
+  }
+
+  return controller_interface::return_type::OK;
 }
 
 bool SpnavController::checkReturnCode(int ret)
 {
-    switch (ret)
-    {
+  switch (ret)
+  {
     case KDL::ChainIkSolverVel_pinv::E_CONVERGE_PINV_SINGULAR:
-        ROS_WARN_THROTTLE(UPDATE_LOG_THROTTLE, "Convergence issue: pseudo-inverse is singular");
-        return false;
+      RCLCPP_WARN_THROTTLE(get_node()->get_logger(), *get_node()->get_clock(), UPDATE_LOG_THROTTLE, "Convergence issue: pseudo-inverse is singular");
+      return false;
     case KDL::SolverI::E_SVD_FAILED:
-        ROS_ERROR_THROTTLE(UPDATE_LOG_THROTTLE, "Convergence issue: SVD failed");
-        return false;
+      RCLCPP_ERROR_THROTTLE(get_node()->get_logger(), *get_node()->get_clock(), UPDATE_LOG_THROTTLE, "Convergence issue: SVD failed");
+      return false;
     case KDL::SolverI::E_NOERROR:
-        return true;
+      return true;
     default:
-        ROS_WARN_THROTTLE(UPDATE_LOG_THROTTLE, "Convergence issue: unknown error");
-        return false;
-    }
+      RCLCPP_WARN_THROTTLE(get_node()->get_logger(), *get_node()->get_clock(), UPDATE_LOG_THROTTLE, "Convergence issue: unknown error");
+      return false;
+  }
 }
 
-void SpnavController::starting(const ros::Time& time)
+void SpnavController::spnavCallback(const sensor_msgs::msg::Joy::SharedPtr msg)
 {
-    std::string out = "Initial arm pose:";
-
-    for (int i = 0; i < armJoints.size(); i++)
-    {
-        q(i) = armJoints[i].getPosition();
-        out += " " + std::to_string(q(i));
-    }
-
-    ROS_INFO("%s", out.c_str());
+  std::lock_guard lock(mtx);
+  std::copy(msg->axes.cbegin(), msg->axes.cend(), joy_axes.begin());
+  std::copy(msg->buttons.cbegin(), msg->buttons.cend(), joy_buttons.begin());
 }
 
-void SpnavController::stopping(const ros::Time& time)
-{ }
-
-void SpnavController::spnavCallback(const sensor_msgs::Joy::ConstPtr& msg)
-{
-    std::lock_guard<std::mutex> lock(mtx);
-    std::copy(msg->axes.cbegin(), msg->axes.cend(), joyAxes.begin());
-    std::copy(msg->buttons.cbegin(), msg->buttons.cend(), joyButtons.begin());
-}
-
-PLUGINLIB_EXPORT_CLASS(tiago_controllers::SpnavController, controller_interface::ControllerBase);
+PLUGINLIB_EXPORT_CLASS(spnav_controller::SpnavController, controller_interface::ControllerInterface);
